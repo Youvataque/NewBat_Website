ee248b57e9351dc4948d7989c590cf146b0be338
SEGUIN Yannis <102596878+Youvataque@users.noreply.github.com>
2024-05-06 21:05:24 +0200

    fin avancement 06

--- src/Components/FondamentalAppComp/Footer.tsx
+++ src/Components/FondamentalAppComp/Footer.tsx
@@ -1,8 +1,9 @@
 /////////////////////////////////////////////////////////////////
 // composant footer
 const Footer:React.FC = () => {
-    return <div className="w-screen h-60 bg-Primary">
-
+    return <div className="w-screen h-60 bg-Primary flex justify-center items-end pb-4">
+        <div className="h-6"/>
+        <p className="text-center text-lg font-normal text-backGround">Créé et designé par Yannis Seguin<br/>©Copyright 2024 - Yannis Seguin</p>
     </div>
 }
 
