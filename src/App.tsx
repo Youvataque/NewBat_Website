09016fb5744aacfea25333dc5aac36cd0b7f96b3
SEGUIN Yannis <102596878+Youvataque@users.noreply.github.com>
2024-05-12 15:49:48 +0200

    fin et hébergement newBat website

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { Route, Routes } from 'react-router'
 import Home from './Views/Home'
 import Pricing from './Views/Pricing'
-import Contact from './Views/Contact'
 import NavBar from './Components/FondamentalAppComp/Navbar.tsx'
 
 function App() {
@@ -11,7 +10,6 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/pricing' element={<Pricing />} />
-        <Route path='/contact' element={<Contact />} />
       </Routes>
     </div>
   )
--- src/Components/FondamentalAppComp/AppBody.tsx
+++
@@ -1,7 +0,0 @@
-const AppBody:React.FC = () => {
-    return <div className="w-screen h-screen flex fle-col justify-center items-start">
-
-    </div>
-}
-
-export default AppBody;
